76fe5236c0d16205d34749cac970069dff49009b
danie <danie@DanielLaptop>
2018-04-16 16:53:45 +0200

    Aufgabe 2 verbessert

--- Aufgabe2/Memory.js
+++ Aufgabe2/Memory.js
@@ -51,9 +51,12 @@ var Aufgabe2;
     // Main Funktion zum Anzeigen der Spielerinfo und dem Memory
     function main() {
         //Popup f�r Kartenpaare
-        numPairs = parseInt(prompt("Anzahl der Kartenpaare (von 5-8 Paaren)"), 10);
-        if (numPairs < 5 || numPairs > 8) {
-            numPairs = 8;
+        gameBoard();
+        function gameBoard() {
+            numPairs = parseInt(prompt("Anzahl der Kartenpaare (von 5-8 Paaren)"), 10);
+            if (numPairs < 5 || numPairs > 8) {
+                gameBoard();
+            }
         }
         //Karten erzeugen
         for (let i = 0; i < numPairs; i++) {
@@ -69,26 +72,29 @@ var Aufgabe2;
         }
         //Spielerinformation
         //Popup f�r Spieleranzahl
-        let eingabeSpieler = prompt("Spieleranzahl eingeben (max. 4 Spieler)" + "");
-        //Initialisierung der verschiedenen F�lle der Spieleranzahl
-        switch (eingabeSpieler) {
-            case "1":
-                numPlayers += 1;
-                break;
-            case "2":
-                numPlayers += 2;
-                break;
-            case "3":
-                numPlayers += 3;
-                break;
-            case "4":
-                numPlayers += 4;
-                break;
-            default:
-                location.reload();
-        }
-        for (let i = 1; i < numPlayers; i++) {
-            spielerDiv(i);
+        playerList();
+        function playerList() {
+            let eingabeSpieler = prompt("Spieleranzahl eingeben (max. 4 Spieler)" + "");
+            //Initialisierung der verschiedenen F�lle der Spieleranzahl
+            switch (eingabeSpieler) {
+                case "1":
+                    numPlayers += 1;
+                    break;
+                case "2":
+                    numPlayers += 2;
+                    break;
+                case "3":
+                    numPlayers += 3;
+                    break;
+                case "4":
+                    numPlayers += 4;
+                    break;
+                default:
+                    playerList();
+            }
+            for (let i = 1; i < numPlayers; i++) {
+                spielerDiv(i);
+            }
         }
         //Anzeigen der Spielerboxen
         function spielerDiv(_numPlayers) {
--- Aufgabe2/Memory.ts
+++ Aufgabe2/Memory.ts
@@ -61,11 +61,13 @@ class Card {
 function main(): void {
 
 //Popup für Kartenpaare
-    numPairs = parseInt(prompt("Anzahl der Kartenpaare (von 5-8 Paaren)"), 10);
-    if (numPairs < 5 || numPairs > 8) { 
-        numPairs = 8;
-    }
-
+    gameBoard();
+    function gameBoard(): void {
+        numPairs = parseInt(prompt("Anzahl der Kartenpaare (von 5-8 Paaren)"), 10);
+        if (numPairs < 5 || numPairs > 8) { 
+            gameBoard();
+        } 
+    }    
 //Karten erzeugen
     for (let i: number = 0; i < numPairs; i++) {
     let card: Card = new Card(cardContent[i]);
@@ -85,33 +87,37 @@ function main(): void {
 //Spielerinformation
     
 //Popup für Spieleranzahl
-    let eingabeSpieler: string = prompt("Spieleranzahl eingeben (max. 4 Spieler)" + "");
-
-//Initialisierung der verschiedenen Fälle der Spieleranzahl
-    switch (eingabeSpieler) {
-            case "1": 
-                numPlayers += 1;
-                break; 
-                    
-            case "2":
-                numPlayers += 2;
-                break;
-                    
-            case "3":
-                numPlayers += 3;
-                break;
-                    
-            case "4":
-                numPlayers += 4;
-                break;
-                    
-            default: 
-                location.reload();
-            }
-            
-    for ( let i: number = 1; i < numPlayers; i++) {
-        spielerDiv(i);
-    }     
+    playerList();
+    function playerList(): void {
+        let eingabeSpieler: string = prompt("Spieleranzahl eingeben (max. 4 Spieler)" + "");
+    
+    //Initialisierung der verschiedenen Fälle der Spieleranzahl
+        switch (eingabeSpieler) {
+                case "1": 
+                    numPlayers += 1;
+                    break; 
+                        
+                case "2":
+                    numPlayers += 2;
+                    break;
+                        
+                case "3":
+                    numPlayers += 3;
+                    break;
+                        
+                case "4":
+                    numPlayers += 4;
+                    break;
+                        
+                default: 
+                    playerList();
+                }
+    
+                
+        for ( let i: number = 1; i < numPlayers; i++) {
+            spielerDiv(i);
+        }
+}     
 
 //Anzeigen der Spielerboxen
     function spielerDiv(_numPlayers: number): void {
